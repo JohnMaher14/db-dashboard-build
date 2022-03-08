17391209184c8798b7c4396c43b4dee7fca6b559
JohnMaher14 <john@digitalbondmena.com>
2022-03-08 18:45:19 +0200

    Auto-generated commit [ci skip]

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,9 +6,10 @@
   ------
   PLACE HERE YOUR OWN JAVASCRIPT CODE IF NEEDED
   WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR JAVASCRIPT CODE PLEASE CONSIDER WRITING YOUR SCRIPT HERE.  */
-  $(".modal button").click(function () {
 
-    $('.modal').modal('hide');
-
-  });
+  window.addEventListener("keydown", function(e) {
+    if(["ArrowUp","ArrowDown"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false);
 })(window);
